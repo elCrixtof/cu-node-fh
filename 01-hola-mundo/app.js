3cf59e23afa7b9315cbe2e2af99744d0f61c3030
Christian <cristian117acosta@hotmail.com>
2022-02-10 21:11:21 -0600

    Fin seccion 1 y 2

--- 01-hola-mundo/app.js
+++ 01-hola-mundo/app.js
@@ -1,1 +1,9 @@
-console.log('hola mundo');
+const nombre  = 'Christian';
+console.log(nombre);
+
+const nombre1 = 'Alan';
+console.log(nombre1);
+
+console.log('hello, motherfuckers!');
+
+console.log('Estoy andando dentro de nodemon');

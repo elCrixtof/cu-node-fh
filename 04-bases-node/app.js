b37840ceca1c8ece47205398636530ae3956673d
Christian <cristian117acosta@hotmail.com>
2022-03-26 00:54:43 -0600

    .

--- 04-bases-node/app.js
+++ 04-bases-node/app.js
@@ -1,15 +1,33 @@
+const { option } = require('yargs');
 const {buildTableFile} = require('./helpers/multiplication')
-const argv = require('yargs').argv;
+const argv = require('yargs')
+    .option('b', {
+        alias: 'base',
+        type: 'number',
+        demandOption: true,
+    })
+    .option('l', {
+        alias: 'listing',
+        type: 'boolean',
+        demandOption: true,
+        default: false    
+    })
+    .check((argv, options) => {
+        if(isNaN(argv.b)) {
+            throw 'The base must be a number';
+        }
+        return true;
+    })
+    .argv;
 
 console.clear();
 
-console.log(process.argv);
 console.log(argv);
-console.log(argv.base);
+// console.log('base: yargs',argv.base);
 
 // const [, , arg3 = 'base=5'] = process.argv;
 // const [, base = 5] = arg3.split('=');
 
-// buildTableFile(base)
-//     .then(fileName => console.log(`The file table-${fileName}.txt has been created`))
-//     .catch(console.log)
+buildTableFile(argv.b, argv.l)
+    .then(fileName => console.log(`The file table-${fileName}.txt has been created`))
+    .catch(console.log)
--- 04-bases-node/helpers/multiplication.js
+++ 04-bases-node/helpers/multiplication.js
@@ -1,16 +1,19 @@
 const fs = require('fs')
 
-const buildTableFile = async(base = 5) => {
+const buildTableFile = async(base = 5, listing = false) => {
     try {
-        console.log('=====================');
-        console.log('    Table of', base);
-        console.log('=====================');
-
         let salida = ``;
         for(let i=1; i<=10; i++) {
             salida += `${base} x ${i} = ${base*i}\n`;
         }
-        console.log(salida);
+
+        if (listing){
+            console.log('=====================');
+            console.log('    Table of', base);
+            console.log('=====================');
+            console.log(salida);
+        }
+        
         fs.writeFileSync(`table-${base}.txt`, salida );
         // fs.writeFile(`table-${base}.txt`, salida , err => {
         //     if(err) throw err;

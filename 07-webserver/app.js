a88a4b855cd2ba0dd1a21f23db45df2501dc3806
Christian <cristian117acosta@hotmail.com>
2022-04-12 01:27:38 -0500

    .

--- 07-webserver/app.js
+++ 07-webserver/app.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 8080; 
 
-app.get('/', function (req, res) {
-    res.send('Home page');
-})
+//Serve content
+app.use( express.static('public') );
 
 app.get('*', function (req, res) {
-    res.send('Page not found');
+    res.sendFile(__dirname + '/public/404.html');
 })
 
 app.listen(PORT, () => {

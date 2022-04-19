4f395950dc5ee2016ada7ebcd2e3542239931959
Christian <cristian117acosta@hotmail.com>
2022-04-19 16:54:38 -0500

    user schema created

---
+++ 08-restserver/models/user.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = Schema ({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    mail: {
+        type: String,
+        required: [true, 'Mail is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    mail: {
+        type: String,
+        unique: true
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    state: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = model( 'User', UserSchema );

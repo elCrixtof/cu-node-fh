1eef187dc3caf6ae35d946f22bf031eb8453f408
Christian <cristian117acosta@hotmail.com>
2022-04-30 14:02:25 -0500

    .

--- 08-restserver/controllers/auth.js
+++ 08-restserver/controllers/auth.js
@@ -1,22 +1,49 @@
 const { response, request }  = require('express');
+const bcryptjs = require('bcryptjs');
+
 const User = require('../models/user');
+const { generarJWT } = require('../helpers/generar-jwt');
 
 const login = async (req = require, res = response) => {
     try {
         const { mail, password } = req.body;
 
-        const user = await User.findOne({mail, password});
+        // Find user by mail 
+        const user = await User.findOne({mail});
+        if (!user) {
+            return res.status(400).json({
+                msg: 'User / Password are not corrects - mail'
+            })
+        }
+
+        // User do exist
+        if ( !user.state ) {
+            return res.status(400).json({
+                msg: `User / Password are not corrects - status: false`
+            })
+        }
+
+        // Verify password
+        const validatePassword = bcryptjs.compareSync(password, user.password);
+        if( !validatePassword ) {
+            return res.status(400).json({
+                msg: 'msg: User / Password are not corrects - password'
+            })
+        }
+
+
+        // Authenticate user and generate password
+        const token = await generarJWT(user.id);
         
         res.json({
-            msg: 'Authetication is ok',
-            mail,
-            password, 
-            user
+            user,
+            token
         });
     } catch (error) {
         // console.log(typeof(error));
         // console.log(error.message);
-        res.json({
+        console.log(error);
+        res.status(500).json({
             msg: error.message,
             type: error.name
         })
